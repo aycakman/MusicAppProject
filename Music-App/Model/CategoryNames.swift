fcbb224dda7ee2af77f01d59e7f0b97eaf58c12c
aycakman <aycaakman0@gmail.com>
2023-05-15 07:59:23 +0300

    Create CategoryNames.swift
    created category names model

---
+++ Music-App/Model/CategoryNames.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryName.swift
+//  Music-App
+//
+//  Created by Ayca Akman on 8.05.2023.
+//
+
+import Foundation
+
+struct CategoryNames : Codable {
+    let data: [ArtistData]
+}
+
+struct ArtistData : Codable {
+    let id: Int
+    let name: String
+    let picture: String
+    let pictureSmall, pictureMedium, pictureBig, pictureXl: String
+    let tracklist: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, picture
+        case pictureSmall = "picture_small"
+        case pictureMedium = "picture_medium"
+        case pictureBig = "picture_big"
+        case pictureXl = "picture_xl"
+        case tracklist
+    }
+}
