50b83f9b2602479f963d4f628d5889cfe940c244
aycakman <aycaakman0@gmail.com>
2023-05-15 08:04:26 +0300

    added artist albums view controllers

---
+++ Music-App/Controller/ArtistAlbumsTableViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  ArtistPageTableViewCell.swift
+//  Music-App
+//
+//  Created by Ayca Akman on 8.05.2023.
+//
+
+import UIKit
+import Kingfisher
+class ArtistAlbumsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var albumNamesLabel: UILabel!
+    @IBOutlet weak var albumDatesLabel: UILabel!
+    @IBOutlet weak var albumImageView: UIImageView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func setup(with album: Album, releaseDate: String?) {
+        albumNamesLabel.text = album.title
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        if let date = dateFormatter.date(from: releaseDate ?? "") {
+            dateFormatter.dateFormat = "dd MMMM yyyy"  // Kullanıcıya göstermek istediğiniz tarih formatı
+            let displayDate = dateFormatter.string(from: date)
+            albumDatesLabel.text = displayDate
+        }
+        let imageUrlString = album.coverMedium
+        let processor = DownsamplingImageProcessor(size: albumImageView.bounds.size)
+        albumImageView.kf.indicatorType = .activity
+        if let imageUrl = URL(string: imageUrlString) {
+            albumImageView.kf.setImage(with: imageUrl,
+                                       placeholder: UIImage(named: "placeholderImage"),
+                                       options: [
+                                        .processor(processor),
+                                        .scaleFactor(UIScreen.main.scale),
+                                        .transition(.fade(0.5)),
+                                        .cacheOriginalImage
+                                       ])
+            
+        }
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+      
+    }
+
+   
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Music-App/Controller/ArtistAlbumsViewController.swift
@@ -0,0 +1,172 @@
+//
+//  ArtistPageViewController.swift
+//  Music-App
+//
+//  Created by Ayca Akman on 8.05.2023.
+//
+
+import UIKit
+
+class ArtistAlbumsViewController: UIViewController{
+    
+    
+    @IBOutlet weak var albumsTableView: UITableView!
+    @IBOutlet weak var artistImageView: UIImageView!
+    
+    var artistName: String?
+    var artistImageUrl: String?
+    
+    var artistPage : ArtistPage!
+    var tracklistData : [TracklistData]!
+    var albums: [Album] = []
+    var albumIDs: [Int] = []
+    var artistID : Int!
+    var selectedRowIndex : Int?
+    var releaseDates: [Int: String] = [:]
+    var releaseDate: String?
+    var tracklist : String! = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        albumsTableView.dataSource = self
+        albumsTableView.delegate = self
+        
+        if let artistImageUrl = artistImageUrl {
+            loadImage(from: artistImageUrl)
+        }
+
+        tracklist = fetchData()
+        fetchDataTracklist(tracklist)
+        
+        showSecondViewController()
+
+    }
+    
+ 
+    func showSecondViewController() {
+        let destinationVC = AlbumDetailsViewController()
+        destinationVC.onReleaseDateReceived = { [weak self] albumID, releaseDate in
+            self?.releaseDates[albumID] = releaseDate
+            DispatchQueue.main.async {
+                self?.albumsTableView.reloadData()
+                
+            }
+        }
+    }
+    
+    func loadImage(from urlString: String) {
+        if let imageUrl = URL(string: urlString) {
+            URLSession.shared.dataTask(with: imageUrl) { (data, response, error) in
+                if let error = error {
+                    print("Error downloading image: \(error)")
+                } else if let data = data {
+                    DispatchQueue.main.async {
+                        self.artistImageView.image = UIImage(data: data)
+                    }
+                }
+            }.resume()
+        }
+    }
+    
+    
+    func fetchData() -> String {
+        let url = URL(string: "https://api.deezer.com/artist/\(artistID!)")!
+        NetworkService().downloadData(url: url) { (artists: ArtistPage?) in
+            if let artists = artists {
+                self.tracklist = artists.tracklist
+                DispatchQueue.main.async {
+                    self.albumsTableView.reloadData()
+                }
+            }
+        }
+        return tracklist!
+    }
+    
+    
+    func fetchDataTracklist(_ tracklist: String) {
+        let url = URL(string: self.tracklist)!
+        NetworkService().downloadData(url: url) { (tracklists: TracklistModel?) in
+            if let tracklists = tracklists {
+                for track in tracklists.data {
+                    let albumId = track.album.id
+                    if !self.albums.contains(where: { $0.id == albumId }) {
+                        self.albums.append(track.album)
+                        self.fetchReleaseDate(for: albumId) { (releaseDate) in
+                            if let releaseDate = releaseDate {
+                                DispatchQueue.main.async {
+                                    self.releaseDates[albumId] = releaseDate
+                                    self.albumsTableView.reloadData()
+                                }
+                            }
+                        }
+                    }
+                }
+                DispatchQueue.main.async {
+                    self.albumsTableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    func fetchReleaseDate(for albumID: Int, completion: @escaping (String?) -> Void) {
+        let url = URL(string: "https://api.deezer.com/album/\(albumID)")
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if let error = error {
+                print("Error fetching release date: \(error)")
+                completion(nil)
+            } else if let data = data {
+                do {
+                    let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+                    if let dictionary = jsonObject as? [String: Any], let releaseDate = dictionary["release_date"] as? String {
+                        print(releaseDate) //buraya releaseDate değeri geliyor
+                        completion(releaseDate)
+                    } else {
+                        print("Error parsing release date.")
+                        completion(nil)
+                    }
+                } catch {
+                    print("Error parsing release date: \(error)")
+                    completion(nil)
+                }
+            }
+        }.resume()
+    }
+
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toGoDetailsOfAlbum" {
+            if let destinationVC = segue.destination as? AlbumDetailsViewController,
+               let index = selectedRowIndex {
+                destinationVC.navigationItem.title = albums[index].title
+                destinationVC.albumID = albums[index].id
+            }
+        }
+    }
+    
+}
+
+extension ArtistAlbumsViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("albums.count: \(albums.count)")
+        return albums.count
+     
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = albumsTableView.dequeueReusableCell(withIdentifier: "artistCell", for: indexPath) as! ArtistAlbumsTableViewCell
+        
+        let album = albums[indexPath.row]
+        let releaseDate = releaseDates[album.id] ?? "error"
+      
+        cell.setup(with: album, releaseDate: releaseDate)
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedRowIndex = indexPath.row
+        performSegue(withIdentifier: "toGoDetailsOfAlbum", sender: nil)
+    }
+    
+}
+
